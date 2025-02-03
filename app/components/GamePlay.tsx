1aec4d699c3950b6c9aafaa61eac297a733e6439
Mads Lee Giil <mads@giil.no>
2025-02-03 11:22:17 +0900

    Create a toggle to show or hide the Hangeul keyboard layout

--- app/components/GamePlay.tsx
+++ app/components/GamePlay.tsx
@@ -1,7 +1,6 @@
 import {KoreanKeyboard} from "~/components/KoreanKeyboard";
 import {ChangeEvent, useState} from "react";
 import {oneBlockWords, pickRandomWord} from "~/.client/words";
-import {newExpression} from "@babel/types";
 
 enum GameState {
     NOT_STARTED,
@@ -17,6 +16,7 @@ export const GamePlay = () => {
     const [words, setWords] = useState<string[]>(oneBlockWords)
     const [currentWord, setCurrentWord] = useState<string | undefined>();
     const [typedWord, setTypedWord] = useState("");
+    const [showKoreanKeyboardLayout, setShowKoreanKeyboardLayout] = useState(true);
 
     const pickNewWord = () => {
         const [randomWord, remainingWords] = pickRandomWord(words);
@@ -47,30 +47,41 @@ export const GamePlay = () => {
                     {wordsFinished} words out of {wordsToFinish} finished!
                 </div>
             }
-            { gameState === GameState.NOT_STARTED &&
+            {gameState === GameState.NOT_STARTED &&
                 <div>
                     <button className="btn" onClick={() => pickNewWord()}>Start game</button>
                 </div>
             }
-            { gameState === GameState.TYPING_WORD &&
+            {gameState === GameState.TYPING_WORD &&
                 <div>
                     <h3 className="text-gray-800">Word to type:</h3>
                     <div className="mt-6 mb-6 text-6xl">{currentWord}</div>
                     <input autoFocus value={typedWord} onChange={handleInput}/>
                 </div>
             }
-            { gameState === GameState.CORRECT_WORD_SUBMITTED &&
+            {gameState === GameState.CORRECT_WORD_SUBMITTED &&
                 <div>
                     <h3>Correct!</h3>
                     <button className="btn" onClick={() => pickNewWord()}>New word</button>
                 </div>
             }
-            { gameState === GameState.FINISHED &&
+            {gameState === GameState.FINISHED &&
                 <div>
                     <h3>Finished!</h3>
                 </div>
             }
-            <KoreanKeyboard/>
+            <div className="mt-12">
+                <label className="inline-flex items-center cursor-pointer">
+                    <input type="checkbox" value={showKoreanKeyboardLayout ? "" : undefined} className="sr-only peer"
+                           onChange={() => setShowKoreanKeyboardLayout(!showKoreanKeyboardLayout)}/>
+                    <div
+                        className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                    <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Show Hangeul keyboard layout</span>
+                </label>
+                {showKoreanKeyboardLayout &&
+                    <KoreanKeyboard/>
+                }
+            </div>
         </div>
     )
 }
